d5ede4ab1443f6702e9c4a37a548892f8be747a7
Jon Zeolla <zeolla@gmail.com>
2025-07-09 22:46:16 -0400

    Better demo

--- presentations/2025-07-BSides-PGH/demo/ai-native-python.sh
+++ presentations/2025-07-BSides-PGH/demo/ai-native-python.sh
@@ -3,11 +3,20 @@
 # shellcheck disable=SC1091
 source ../../../modules/shared/demo/demo-magic.sh
 
+docker_hub="https://hub.docker.com/repository/docker/jonzeolla/bsidespgh25/general"
+echo "# Ok, and now let's look at ${docker_hub}"
+open "${docker_hub}"
+
 export ALLOW_FORCE_PUSH=true
 rm -rf bsidespgh25 2>/dev/null || true
 pe "uvx --with gitpython cookiecutter git+ssh://git@github.com/zenable-io/ai-native-python.git project_name=bsidespgh25"
 # pe "uvx --with gitpython cookiecutter git+ssh://git@github.com/zenable-io/ai-native-python.git prjoect_name=bsidespgh25 --checkout TODO --keep-project-on-failure"
+#
+github=https://github.com/JonZeolla/bsidespgh25
+echo "# Let's take a quick look at ${github}"
+open "${github}"
 wait
+
 
 # We want this to be single quoted and not expanded
 # shellcheck disable=SC2016
@@ -15,25 +24,18 @@ pe 'cd $(ls -td * | head -1)'
 task clean
 
 pe "grep -r NotImplementedError *"
-wait
 
 pe "task lint"
-wait
 
 task clean
 export IGNORES="-I .git -I .venv -I .task -I .ruff_cache"
 pe "tree $IGNORES -a"
-wait
 
 pe "head -5 .github/workflows/*.yml .github/actions/*/*.yml"
-wait
 
 pe "PLATFORM=all task build"
-wait
 
 pe "task unit-test"
-wait
 
 pe "task sbom vulnscan license-check"
 pe "ls sbom* vulns* license*"
-wait
